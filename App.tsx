0442fba67516d90cc8db80067ceed9bd8619ef55
citReyJoshua <reyjoshua.macarat@cit.edu>
2022-02-21 06:44:27 +0800

    chore: setup folder aliases

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { AxieURI } from '@utils/constants'
+
+const client = new ApolloClient({
+  uri: AxieURI,
+  cache: new InMemoryCache(),
+  defaultOptions: { watchQuery: { fetchPolicy: 'cache-and-network' } },
+})
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   )
 }
 
